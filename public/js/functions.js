533470636897d1c75cc7a790909d773e9c314f59
Michael Mallari <michael@michaelmallari.com>
2016-09-10 00:24:23 -0400

    Added basic template

---
+++ public/js/functions.js
@@ -0,0 +1,302 @@
+$(document).ready(function() {
+	if ($(window).width() < 375) {
+// alert("Window width < 375");
+
+	}
+	if ($(window).width() >= 375) {
+// alert("Window width >= 375");
+
+	} 
+	if ($(window).width() >= 375 && $(window).width() <= 413) {
+// alert("Window width >= 375 + <= 413");
+
+	}
+	if ($(window).width() <= 413) {
+// alert("Window width <= 413");
+
+	}
+	if ($(window).width() >= 414) {
+// alert("Window width >= 414");
+
+	} 
+	if ($(window).width() >= 414 && $(window).width() <= 506) {
+// alert("Window width >= 414 + <= 506");
+
+	}
+	if ($(window).width() <= 506) {
+// alert("Window width <= 413");
+
+	}
+	if ($(window).width() >= 507) {
+// alert("Window width >= 507");
+
+	} 
+	if ($(window).width() >= 507 && $(window).width() <= 677) {
+// alert("Window width >= 507 + <= 677");
+
+	}
+	if ($(window).width() <= 677) {
+// alert("Window width <= 677");
+
+	}
+	if ($(window).width() >= 678) {
+// alert("Window width >= 678");
+
+	}
+	if ($(window).width() >= 678 && $(window).width() <= 767) {
+// alert("Window width >= 678 + <= 767");
+
+	}
+	if ($(window).width() <= 767) {
+// alert("Window width <= 767");
+
+	}
+	if ($(window).width() >= 768) {
+// alert("Window width >= 768");
+
+	}
+	if ($(window).width() >= 768 && $(window).width() <= 991) {
+// alert("Window width >= 768 + <= 991");
+
+	}
+	if ($(window).width() <= 991) {
+// alert("Window width <= 991");
+
+	}
+	if ($(window).width() >= 992) {
+// alert("Window width >= 992");
+
+	}
+	if ($(window).width() >= 992 && $(window).width() <= 1023) {
+// alert("Window width >= 992 + <= 1199");
+
+	}
+	if ($(window).width() <= 1023) {
+// alert("Window width <= 1023");
+
+	}
+	if ($(window).width() >= 1024) {
+// alert("Window width >= 1024");
+
+	}
+	if ($(window).width() >= 1024 && $(window).width() <= 1199) {
+// alert("Window width >= 1024 + <= 1199");
+
+	}
+	if ($(window).width() <= 1199) {
+// alert("Window width <= 1199");
+
+	}
+	if ($(window).width() >= 1200) {
+// alert("Window width >= 1200");
+
+	}
+});
+
+
+
+
+
+
+
+
+
+
+$(window).resize(function(){
+	if ($(window).width() < 375) {
+// alert("Window width < 375");
+
+	}
+	if ($(window).width() >= 375) {
+// alert("Window width >= 375");
+
+	} 
+	if ($(window).width() >= 375 && $(window).width() <= 413) {
+// alert("Window width >= 375 + <= 413");
+
+	}
+	if ($(window).width() <= 413) {
+// alert("Window width <= 413");
+
+	}
+	if ($(window).width() >= 414) {
+// alert("Window width >= 414");
+
+	} 
+	if ($(window).width() >= 414 && $(window).width() <= 506) {
+// alert("Window width >= 414 + <= 506");
+
+	}
+	if ($(window).width() <= 506) {
+// alert("Window width <= 413");
+
+	}
+	if ($(window).width() >= 507) {
+// alert("Window width >= 507");
+
+	} 
+	if ($(window).width() >= 507 && $(window).width() <= 677) {
+// alert("Window width >= 507 + <= 677");
+
+	}
+	if ($(window).width() <= 677) {
+// alert("Window width <= 677");
+
+	}
+	if ($(window).width() >= 678) {
+// alert("Window width >= 678");
+
+	}
+	if ($(window).width() >= 678 && $(window).width() <= 767) {
+// alert("Window width >= 678 + <= 767");
+
+	}
+	if ($(window).width() <= 767) {
+// alert("Window width <= 767");
+
+	}
+	if ($(window).width() >= 768) {
+// alert("Window width >= 768");
+
+	}
+	if ($(window).width() >= 768 && $(window).width() <= 991) {
+// alert("Window width >= 768 + <= 991");
+
+	}
+	if ($(window).width() <= 991) {
+// alert("Window width <= 991");
+
+	}
+	if ($(window).width() >= 992) {
+// alert("Window width >= 992");
+
+	}
+	if ($(window).width() >= 992 && $(window).width() <= 1023) {
+// alert("Window width >= 992 + <= 1199");
+
+	}
+	if ($(window).width() <= 1023) {
+// alert("Window width <= 1023");
+
+	}
+	if ($(window).width() >= 1024) {
+// alert("Window width >= 1024");
+
+	}
+	if ($(window).width() >= 1024 && $(window).width() <= 1199) {
+// alert("Window width >= 1024 + <= 1199");
+
+	}
+	if ($(window).width() <= 1199) {
+// alert("Window width <= 1199");
+
+	}
+	if ($(window).width() >= 1200) {
+// alert("Window width >= 1200");
+
+	}
+}).resize();
+
+
+
+
+
+
+
+
+
+
+$(window).scroll(function() {
+	if ($(window).width() < 375) {
+// alert("Window width < 375");
+
+	}
+	if ($(window).width() >= 375) {
+// alert("Window width >= 375");
+
+	} 
+	if ($(window).width() >= 375 && $(window).width() <= 413) {
+// alert("Window width >= 375 + <= 413");
+
+	}
+	if ($(window).width() <= 413) {
+// alert("Window width <= 413");
+
+	}
+	if ($(window).width() >= 414) {
+// alert("Window width >= 414");
+
+	} 
+	if ($(window).width() >= 414 && $(window).width() <= 506) {
+// alert("Window width >= 414 + <= 506");
+
+	}
+	if ($(window).width() <= 506) {
+// alert("Window width <= 413");
+
+	}
+	if ($(window).width() >= 507) {
+// alert("Window width >= 507");
+
+	} 
+	if ($(window).width() >= 507 && $(window).width() <= 677) {
+// alert("Window width >= 507 + <= 677");
+
+	}
+	if ($(window).width() <= 677) {
+// alert("Window width <= 677");
+
+	}
+	if ($(window).width() >= 678) {
+// alert("Window width >= 678");
+
+	}
+	if ($(window).width() >= 678 && $(window).width() <= 767) {
+// alert("Window width >= 678 + <= 767");
+
+	}
+	if ($(window).width() <= 767) {
+// alert("Window width <= 767");
+
+	}
+	if ($(window).width() >= 768) {
+// alert("Window width >= 768");
+
+	}
+	if ($(window).width() >= 768 && $(window).width() <= 991) {
+// alert("Window width >= 768 + <= 991");
+
+	}
+	if ($(window).width() <= 991) {
+// alert("Window width <= 991");
+
+	}
+	if ($(window).width() >= 992) {
+// alert("Window width >= 992");
+
+	}
+	if ($(window).width() >= 992 && $(window).width() <= 1023) {
+// alert("Window width >= 992 + <= 1199");
+
+	}
+	if ($(window).width() <= 1023) {
+// alert("Window width <= 1023");
+
+	}
+	if ($(window).width() >= 1024) {
+// alert("Window width >= 1024");
+
+	}
+	if ($(window).width() >= 1024 && $(window).width() <= 1199) {
+// alert("Window width >= 1024 + <= 1199");
+
+	}
+	if ($(window).width() <= 1199) {
+// alert("Window width <= 1199");
+
+	}
+	if ($(window).width() >= 1200) {
+// alert("Window width >= 1200");
+
+	}
+});
